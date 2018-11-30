d36076d00aebbf5fd8994d6587e13c690e13f651
Tiago Lobato <33132633+ticolobato@users.noreply.github.com>
2018-11-30 09:44:20 -0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <param name="disposing">
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        private void InitializeComponent()
+        {
+            this.Imagem = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.Imagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Imagem
+            // 
+            this.Imagem.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Imagem.Location = new System.Drawing.Point(325, 12);
+            this.Imagem.Name = "Imagem";
+            this.Imagem.Size = new System.Drawing.Size(482, 424);
+            this.Imagem.TabIndex = 0;
+            this.Imagem.TabStop = false;
+            this.Imagem.Click += new System.EventHandler(this.Imagem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gerador de Curvas de Bezier";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.radioButton1.Location = new System.Drawing.Point(28, 138);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(173, 26);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Setar Ponto Inicial";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.radioButton2.Location = new System.Drawing.Point(28, 199);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(167, 26);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.Text = "Setar Ponto Final";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.radioButton3.Location = new System.Drawing.Point(28, 263);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(236, 26);
+            this.radioButton3.TabIndex = 19;
+            this.radioButton3.Text = "Setar Ponto de Controle 1";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.radioButton4.Location = new System.Drawing.Point(28, 329);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(236, 26);
+            this.radioButton4.TabIndex = 20;
+            this.radioButton4.Text = "Setar Ponto de Controle 2";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 448);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Imagem);
+            this.Name = "Form1";
+            this.Text = "Curva Bezier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Imagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Imagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private PointF[] Points = new PointF[4];
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(Imagem.Height, Imagem.Width);
+            Imagem.Image = bmp;
+
+        }
+
+        
+        private static float calcularX(float u, float X0, float X1, float X2, float X3)
+        {
+            double resp1 = System.Convert.ToDouble(X0) * Math.Pow((1 - u), 3) + System.Convert.ToDouble(X1) * 3 * u * Math.Pow((1 - u), 2) + System.Convert.ToDouble(X2) * 3 * Math.Pow(u, 2) * (1 - u) + System.Convert.ToDouble(X3) * Math.Pow(u, 3);
+            float resp = (float)resp1;
+            return resp;
+            
+        }
+        private static float calcularY(float u, float Y0, float Y1, float Y2, float Y3)
+        {
+            double resp1 = System.Convert.ToDouble(Y0) * Math.Pow((1 - u), 3) + System.Convert.ToDouble(Y1) * 3 * u * Math.Pow((1 - u), 2) + System.Convert.ToDouble(Y2) * 3 * Math.Pow(u, 2) * (1 - u) + System.Convert.ToDouble(Y3) * Math.Pow(u, 3);
+            float resp = (float)resp1;
+            return resp;
+
+        }
+
+        // Draw the Bezier curve.
+        public static void DesenharCurva(Graphics gr, Pen the_pen,float dt, PointF pt0, PointF pt1, PointF pt2, PointF pt3)
+        {
+           
+            // Draw the curve.
+            List<PointF> points = new List<PointF>();
+            for (float t = 0.0f; t < 1.0; t += dt)
+            {
+                points.Add(new PointF(
+                    calcularX(t, pt0.X, pt1.X, pt2.X, pt3.X),
+                    calcularY(t, pt0.Y, pt1.Y, pt2.Y, pt3.Y)));
+            }
+
+            // Connect to the final point.
+            points.Add(new PointF(
+                calcularX(1.0f, pt0.X, pt1.X, pt2.X, pt3.X),
+                calcularY(1.0f, pt0.Y, pt1.Y, pt2.Y, pt3.Y)));
+
+            
+            gr.DrawLines(the_pen, points.ToArray());
+            
+
+        }
+
+        // Draw the currently selected points. 
+        // If we have four points, draw the Bezier curve.
+        private void Desenhar(object sender, EventArgs f)
+        {
+            
+            Bitmap bmp = new Bitmap(Imagem.Height, Imagem.Width);
+            using (Graphics e = Graphics.FromImage(bmp))
+            {
+                    e.SmoothingMode = SmoothingMode.AntiAlias;
+                    e.Clear(Imagem.BackColor);
+
+
+                    DesenharCurva(e,Pens.Black, 0.01f,
+                        Points[0], Points[1], Points[2], Points[3]);
+                
+                
+                // Draw the control points.
+                for (int i = 0; i < 4; i++)
+                {
+                    e.FillRectangle(Brushes.Black,
+                        Points[i].X - 3, Points[i].Y - 3, 3, 3);
+                    e.DrawRectangle(Pens.Black,
+                        Points[i].X - 3, Points[i].Y - 3, 3, 3);
+                }
+            }
+            
+            Imagem.Image = bmp;
+        }
+
+
+        private void DesenharPonto(object sender, EventArgs f, int x, int y)
+        {
+
+            Bitmap bmp = (Bitmap)Imagem.Image;
+            using (Graphics e = Graphics.FromImage(bmp))
+            {
+
+             e.FillRectangle(Brushes.Black,
+                        x - 3, y - 3, 3, 3);
+             e.DrawRectangle(Pens.Black,
+                        x - 3, y - 3, 3, 3);
+            }
+
+            Imagem.Image = bmp;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        int cont = 0; 
+        private void Imagem_Click(object sender, EventArgs e)
+        {
+            
+
+            MouseEventArgs mouse = (MouseEventArgs)e;
+
+            if (radioButton1.Checked == true)
+            {
+                Points[0].X = mouse.X;
+                Points[0].Y = mouse.Y;
+                DesenharPonto(sender, e, mouse.X, mouse.Y);
+                cont++;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                Points[3].X = mouse.X;
+                Points[3].Y = mouse.Y;
+                DesenharPonto(sender, e, mouse.X, mouse.Y);
+                cont++;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                Points[1].X = mouse.X;
+                Points[1].Y = mouse.Y;
+                DesenharPonto(sender, e, mouse.X, mouse.Y);
+                cont++;
+            }
+            else if (radioButton4.Checked == true)
+            {
+                Points[2].X = mouse.X;
+                Points[2].Y = mouse.Y;
+                DesenharPonto(sender, e, mouse.X, mouse.Y);
+                cont++;
+            }
+
+            if (cont > 3)
+            {
+                Desenhar(sender, e);
+            }
+
+        }
+    }
+}
